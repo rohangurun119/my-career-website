1430abc9201c7162745b5f369bad5766c68ed2f4
rohangurun119 <151045327+rohangurun119@users.noreply.github.com>
2025-06-27 08:05:04 +0000

    added the flask project

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/chinese')
+def chinese():
+    return '你好，世界！'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
